d487b0c4710722d49e46e3d31d43cd6511377dda
Ketan Bhavsar <mr.ketanbhavsar@gmail.com>
2024-05-24 20:11:20 +0530

    chore: Update home page with bottom navigation bar
    This commit adds a bottom navigation bar to the home page in the EASEHomePage widget. The bottom navigation bar allows users to easily switch between different sections of the app, improving navigation and user experience.

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -8,6 +8,14 @@ class EASEHomePage extends StatefulWidget {
 }
 
 class _EASEHomePageState extends State<EASEHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +32,33 @@ class _EASEHomePageState extends State<EASEHomePage> {
           // Add your onPressed function here
         },
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Theme.of(context).colorScheme.secondary,
+        enableFeedback: true,
+        currentIndex: _selectedIndex,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money_outlined),
+            label: "Sale",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: "Purchase",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money_off_outlined),
+            label: "Expense",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.done_all_outlined),
+            label: "Payment",
+          ),
+        ],
+        onTap: (value) => _onItemTapped(value),
       ),
     );
   }
