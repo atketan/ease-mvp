882807850f286c90fc0e3a1fdbf418ccd5226aa7
Ketan Bhavsar <mr.ketanbhavsar@gmail.com>
2024-09-04 12:23:09 +0530

    Minor fixes on the typeaheadfield for entity selection

--- lib/features/invoice_manager/widgets/entity_type_ahead_field.dart
+++ lib/features/invoice_manager/widgets/entity_type_ahead_field.dart
@@ -27,6 +27,7 @@ class EntityTypeAheadField extends StatefulWidget {
 
 class _EntityTypeAheadFieldState extends State<EntityTypeAheadField> {
   final TextEditingController _controller = TextEditingController();
+  bool _isEditing = true;
 
   @override
   void dispose() {
@@ -38,88 +39,126 @@ class _EntityTypeAheadFieldState extends State<EntityTypeAheadField> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: TypeAheadField<Entity>(
-        builder: (context, controller, focusNode) {
-          return TextField(
-            controller: controller,
-            focusNode: focusNode,
-            onTapOutside: (event) => focusNode.unfocus(),
-            decoration: InputDecoration(
-              labelText: widget.invoiceType == InvoiceType.Sales
-                  ? 'Add Customer'
-                  : 'Add Vendor',
-              hintText:
-                  'Start typing to search or add ${widget.invoiceType == InvoiceType.Sales ? 'Customer' : 'Vendor'}',
-            ),
-            style: Theme.of(context).textTheme.titleMedium,
-          );
-        },
-        suggestionsCallback: (pattern) async {
-          if (pattern.isEmpty) return <Entity>[];
-          if (widget.invoiceType == InvoiceType.Sales) {
-            final customers = await CustomersDAO().getAllCustomers();
-            final matchedCustomers = customers
-                .where((customer) =>
-                    customer.name.toLowerCase().contains(pattern.toLowerCase()))
-                .map((c) => Entity(id: c.id, name: c.name))
-                .toList();
-            if (matchedCustomers.isEmpty) {
-              return [Entity(id: -1, name: "Add Customer: $pattern")];
-            }
-            return matchedCustomers;
-          } else {
-            final vendors = await VendorsDAO().getAllVendors();
-            final matchedVendors = vendors
-                .where((vendor) =>
-                    vendor.name.toLowerCase().contains(pattern.toLowerCase()))
-                .map((v) => Entity(id: v.id, name: v.name))
-                .toList();
-            if (matchedVendors.isEmpty) {
-              return [Entity(id: -1, name: "Add Vendor: $pattern")];
-            }
-            return matchedVendors;
-          }
-        },
-        itemBuilder: (context, suggestion) {
-          return ListTile(
-            title: Text(suggestion.name),
-          );
-        },
-        onSelected: (suggestion) {
-          debugPrint('Suggestion: ${suggestion.id}, ${suggestion.name}');
-          if (suggestion.id == -1) {
-            // Add new entity
-            if (widget.invoiceType == InvoiceType.Sales) {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => UpdateCustomersPage(
-                    mode: CustomersFormMode.Add,
-                  ),
-                ),
-              );
-            } else {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => UpdateVendorsPage(
-                    mode: VendorsFormMode.Add,
-                  ),
-                ),
-              );
-            }
-          } else {
-            // Select existing entity
-            if (suggestion.id != null) {
-              if (widget.invoiceType == InvoiceType.Sales) {
-                context
-                    .read<InvoiceManagerCubit>()
-                    .setCustomerId(suggestion.id!);
-              } else {
-                context.read<InvoiceManagerCubit>().setVendorId(suggestion.id!);
-              }
-            }
-          }
-        },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.invoiceType == InvoiceType.Sales ? 'Customer' : 'Vendor',
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+          SizedBox(height: 8),
+          _isEditing ? _buildTypeAheadField() : _buildSelectedEntityField(),
+        ],
       ),
+    );
+  }
+
+  Widget _buildTypeAheadField() {
+    return TypeAheadField<Entity>(
+      builder: (context, controller, focusNode) {
+        return TextField(
+          controller: controller,
+          focusNode: focusNode,
+          onTapOutside: (event) => focusNode.unfocus(),
+          decoration: InputDecoration(
+            labelText: widget.invoiceType == InvoiceType.Sales
+                ? 'Select customer'
+                : 'Select vendor',
+            hintText:
+                'Start typing to search or add ${widget.invoiceType == InvoiceType.Sales ? 'Customer' : 'Vendor'}',
+          ),
+          style: Theme.of(context).textTheme.titleMedium,
+        );
+      },
+      suggestionsCallback: (pattern) async {
+        if (pattern.isEmpty) return <Entity>[];
+        if (widget.invoiceType == InvoiceType.Sales) {
+          final customers = await CustomersDAO().getAllCustomers();
+          final matchedCustomers = customers
+              .where((customer) =>
+                  customer.name.toLowerCase().contains(pattern.toLowerCase()))
+              .map((c) => Entity(id: c.id, name: c.name))
+              .toList();
+          if (matchedCustomers.isEmpty) {
+            return [Entity(id: -1, name: "Add: $pattern")];
+          }
+          return matchedCustomers;
+        } else {
+          final vendors = await VendorsDAO().getAllVendors();
+          final matchedVendors = vendors
+              .where((vendor) =>
+                  vendor.name.toLowerCase().contains(pattern.toLowerCase()))
+              .map((v) => Entity(id: v.id, name: v.name))
+              .toList();
+          if (matchedVendors.isEmpty) {
+            return [Entity(id: -1, name: "Add: $pattern")];
+          }
+          return matchedVendors;
+        }
+      },
+      itemBuilder: (context, suggestion) {
+        return ListTile(
+          title: Text(suggestion.name),
+        );
+      },
+      onSelected: (suggestion) {
+        setState(() {
+          _controller.text = suggestion.name.split(' ').last;
+          _isEditing = false;
+        });
+        if (suggestion.id != -1 && suggestion.id != null) {
+          if (widget.invoiceType == InvoiceType.Sales) {
+            context.read<InvoiceManagerCubit>().setCustomerId(suggestion.id!);
+          } else {
+            context.read<InvoiceManagerCubit>().setVendorId(suggestion.id!);
+          }
+        } else {
+          // Handle adding new entity
+          if (widget.invoiceType == InvoiceType.Sales) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => UpdateCustomersPage(
+                  mode: CustomersFormMode.Add,
+                ),
+              ),
+            );
+          } else {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => UpdateVendorsPage(
+                  mode: VendorsFormMode.Add,
+                ),
+              ),
+            );
+          }
+          setState(() {
+            _isEditing = true;
+          });
+        }
+      },
+    );
+  }
+
+  Widget _buildSelectedEntityField() {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            _controller.text,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.edit),
+          onPressed: () {
+            setState(() {
+              _isEditing = true;
+            });
+          },
+        ),
+      ],
     );
   }
 }
