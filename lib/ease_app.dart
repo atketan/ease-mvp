6b95e0fc33e9707d69ca2f876befead7b84071e0
Ketan Bhavsar <mr.ketanbhavsar@gmail.com>
2024-09-08 16:14:44 +0530

    Modify the theme to use black color

--- lib/ease_app.dart
+++ lib/ease_app.dart
@@ -1,5 +1,4 @@
 import 'package:ease/features/account/presentation/bloc/login_cubit.dart';
-// import 'package:ease/features/account/presentation/login_page.dart';
 import 'package:ease/features/home/presentation/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -30,49 +29,51 @@ class _EASEAppState extends State<EASEApp> {
         theme: ThemeData(
           fontFamily: 'Lato',
           colorScheme: ColorScheme.fromSeed(
-            seedColor: Color(0xFF00695C), // Dark teal
-            primary: Color(0xFF00695C),
-            secondary: Color(0xFF00897B),
+            seedColor: Colors.black,
+            primary: Colors.black,
+            secondary: Color(0xFF448AFF), // Ink blue
             surface: Colors.white,
-            surfaceContainer: Color(0xFFF5F5F5),
+            onSurface: Colors.black87,
           ),
           useMaterial3: true,
           appBarTheme: AppBarTheme(
-            backgroundColor: Color(0xFF00695C),
+            backgroundColor: Colors.black,
             foregroundColor: Colors.white,
             elevation: 2,
             toolbarHeight: 50.0,
             titleTextStyle: TextStyle(
               fontFamily: 'Lato',
               fontSize: 18,
-              // fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
-            iconTheme: IconThemeData(color: Colors.white),
+            iconTheme: IconThemeData(color: Color(0xFF448AFF)),
           ),
           tabBarTheme: TabBarTheme(
             labelColor: Colors.white,
-            unselectedLabelColor: Colors.grey[400],
+            unselectedLabelColor: Colors.grey[600],
           ),
           listTileTheme: ListTileThemeData(
             visualDensity: VisualDensity.compact,
-            tileColor: Colors.white,
+            // tileColor: Colors.white,
             contentPadding: EdgeInsets.all(8),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
+            iconColor: Color(0xFF448AFF),
+            // textColor: Colors.black87,
           ),
           cardTheme: CardTheme(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
-            color: Colors.white,
-            surfaceTintColor: Colors.white,
+            // color: Colors.white,
+            // surfaceTintColor: Colors.white,
             elevation: 2,
           ),
           textButtonTheme: TextButtonThemeData(
             style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all(Color(0xFF00897B)),
-              foregroundColor: WidgetStateProperty.all(Colors.white),
+              backgroundColor: WidgetStateProperty.all(Colors.black),
+              foregroundColor: WidgetStateProperty.all(Color(0xFF448AFF)),
               shape: WidgetStateProperty.all(
                 RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(4),
@@ -86,68 +87,83 @@ class _EASEAppState extends State<EASEApp> {
           inputDecorationTheme: InputDecorationTheme(
             border: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Color(0xFF00897B),
+                color: Colors.black54,
                 width: 1.0,
               ),
             ),
             enabledBorder: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Color(0xFF00897B),
+                color: Colors.black54,
                 width: 1.0,
               ),
             ),
             focusedBorder: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Color(0xFF00695C),
+                color: Color(0xFF448AFF),
                 width: 2.0,
               ),
             ),
+            labelStyle: TextStyle(color: Colors.black87),
+            hintStyle: TextStyle(color: Colors.black54),
           ),
+          textTheme: TextTheme(
+            bodyLarge: TextStyle(color: Colors.black87),
+            bodyMedium: TextStyle(color: Colors.black87),
+            titleLarge: TextStyle(color: Colors.black),
+            titleMedium: TextStyle(color: Colors.black),
+            titleSmall: TextStyle(color: Colors.black87),
+          ),
+          iconTheme: IconThemeData(color: Color(0xFF448AFF)),
         ),
         darkTheme: ThemeData(
           fontFamily: 'Lato',
           colorScheme: ColorScheme.fromSeed(
-            seedColor: Color(0xFF00695C),
-            primary: Color(0xFF00897B),
-            secondary: Color(0xFF26A69A),
+            seedColor: Colors.black,
+            primary: Colors.white,
+            secondary: Color(0xFF82B1FF), // Lighter ink blue for dark theme
             surface: Color(0xFF121212),
-            surfaceContainer: Color(0xFF121212),
+            // Remove background: Color(0xFF121212),
+            onSurface: Colors.white70,
           ),
           useMaterial3: true,
           appBarTheme: AppBarTheme(
-            backgroundColor: Color(0xFF00695C),
-            foregroundColor: Colors.white,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
             elevation: 2,
+            toolbarHeight: 50.0,
             titleTextStyle: TextStyle(
               fontFamily: 'Lato',
-              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              color: Colors.white,
             ),
-            iconTheme: IconThemeData(color: Colors.white),
+            iconTheme: IconThemeData(color: Color(0xFF82B1FF)),
           ),
           tabBarTheme: TabBarTheme(
-            labelColor: Colors.white,
+            labelColor: Colors.black,
             unselectedLabelColor: Colors.grey[400],
           ),
           listTileTheme: ListTileThemeData(
             visualDensity: VisualDensity.compact,
-            tileColor: Color(0xFF1E1E1E),
+            // tileColor: Color(0xFF1E1E1E),
             contentPadding: EdgeInsets.all(8),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
+            iconColor: Color(0xFF82B1FF),
+            // textColor: Colors.white70,
           ),
           cardTheme: CardTheme(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
-            color: Color(0xFF1E1E1E),
-            surfaceTintColor: Color(0xFF1E1E1E),
+            // color: Color(0xFF1E1E1E),
+            // surfaceTintColor: Color(0xFF1E1E1E),
             elevation: 2,
           ),
           textButtonTheme: TextButtonThemeData(
             style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all(Color(0xFF00897B)),
-              foregroundColor: WidgetStateProperty.all(Colors.white),
+              backgroundColor: WidgetStateProperty.all(Colors.white),
+              foregroundColor: WidgetStateProperty.all(Color(0xFF82B1FF)),
               shape: WidgetStateProperty.all(
                 RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(4),
@@ -161,23 +177,33 @@ class _EASEAppState extends State<EASEApp> {
           inputDecorationTheme: InputDecorationTheme(
             border: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Color(0xFF26A69A),
+                color: Colors.white54,
                 width: 1.0,
               ),
             ),
             enabledBorder: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Color(0xFF26A69A),
+                color: Colors.white54,
                 width: 1.0,
               ),
             ),
             focusedBorder: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Color(0xFF00897B),
+                color: Color(0xFF82B1FF),
                 width: 2.0,
               ),
             ),
+            labelStyle: TextStyle(color: Colors.white70),
+            hintStyle: TextStyle(color: Colors.white54),
           ),
+          textTheme: TextTheme(
+            bodyLarge: TextStyle(color: Colors.white70),
+            bodyMedium: TextStyle(color: Colors.white70),
+            titleLarge: TextStyle(color: Colors.white),
+            titleMedium: TextStyle(color: Colors.white),
+            titleSmall: TextStyle(color: Colors.white70),
+          ),
+          iconTheme: IconThemeData(color: Color(0xFF82B1FF)),
         ),
         home: (_auth.currentUser != null) ? EASEHomePage() : EASEHomePage(),
       ),
